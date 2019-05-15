632e027ee0dbb289541bf6e70c3b0c537f9abb7d
pengleichen <chenpenglei000@yahoo.com>
2019-05-15 21:45:55 +0800

    antd

---
+++ 15.react_ui/antd-demo/src/components/App.js
@@ -0,0 +1,21 @@
+import React, {Component} from 'react'
+
+import {Button} from 'antd'
+
+import 'antd/dist/antd.css'
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Button type="primary">Primary</Button>
+        <Button>Default</Button>
+        <Button type="dashed">Dashed</Button>
+        <Button type="danger">Danger</Button>
+        <Button type="link">Link</Button>
+      </div>
+    )
+  }
+}
+
+export default App
