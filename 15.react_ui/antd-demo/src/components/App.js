ad4e8b38711842a795bf7215b2e8b8279733e020
pengleichen <chenpenglei000@yahoo.com>
2019-05-15 23:57:20 +0800

    loading-on-demand

--- 15.react_ui/antd-demo/src/components/App.js
+++ 15.react_ui/antd-demo/src/components/App.js
@@ -2,7 +2,26 @@ import React, {Component} from 'react'
 
 import {Button} from 'antd'
 
-import 'antd/dist/antd.css'
+//import 'antd/dist/antd.css'
+/*
+可以按需加载CSS样式：
+1. 执行yarn eject
+2. 删除node_modules之后再次执行 yarn install
+3. 执行 yarn add babel-plugin-import
+4. 修改config/webpack.config.js中的babel-loader中的options，添加一个plugins解析
+5. 修改如下：
+    plugins: [
+    ...,
+      [
+        require.resolve('babel-plugin-import'),
+        {
+          libraryName: 'antd',
+          style: 'css'
+        }
+      ],
+      ...
+    ]
+ */
 
 class App extends Component {
   render() {
